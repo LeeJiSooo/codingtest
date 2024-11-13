80779cf4b7c46fab04fe3d11653219baaba1ce94
LeeJiSoo <ljs08010@gmail.com>
2024-11-13 17:28:05 +0900

    BJ_1000

---
+++ src/bronze/BJ_1000.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class BJ_1000 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner scan = new Scanner(System.in);
+        A = scan.nextInt();
+        B = scan.nextInt();
+        System.out.println(A + B);
+    }
+}
