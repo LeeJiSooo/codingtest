bbf34859b055f2c3791fbfea049f2adbe182bf9b
LeeJiSoo <ljs08010@gmail.com>
2024-11-14 10:35:08 +0900

    BJ_1001

---
+++ src/bronze/BJ_1001.java
@@ -0,0 +1,17 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class BJ_1001 {
+    public static void main(String[] args) {
+        int A, B;
+        Scanner scan = new Scanner(System.in);
+
+        A = scan.nextInt();
+        B = scan.nextInt();
+
+        if(0 < A && B < 10){
+            System.out.println(A - B);
+        }
+    }
+}
